b09ffb81df7b9c78de4d4e63a2be65d4e052be2b
Sistemas <Sistemas@SYSTEM-DDSS>
2023-04-12 14:14:29 -0500

    Agregar archivos de proyecto.

---
+++ XtraForm1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WinPruebas
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttstart = new DevExpress.XtraEditors.SimpleButton();
+            this.bttstop = new DevExpress.XtraEditors.SimpleButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bttstart
+            // 
+            this.bttstart.Location = new System.Drawing.Point(155, 34);
+            this.bttstart.Name = "bttstart";
+            this.bttstart.Size = new System.Drawing.Size(75, 23);
+            this.bttstart.TabIndex = 0;
+            this.bttstart.Text = "Iniciar";
+            this.bttstart.Click += new System.EventHandler(this.bttstart_Click);
+            // 
+            // bttstop
+            // 
+            this.bttstop.Location = new System.Drawing.Point(435, 34);
+            this.bttstop.Name = "bttstop";
+            this.bttstop.Size = new System.Drawing.Size(75, 23);
+            this.bttstop.TabIndex = 1;
+            this.bttstop.Text = "Finalizar";
+            this.bttstop.Click += new System.EventHandler(this.bttstop_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(28, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(298, 151);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Location = new System.Drawing.Point(350, 123);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Size = new System.Drawing.Size(289, 150);
+            this.memoEdit1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(321, 33);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "simpleButton1";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 408);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.memoEdit1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.bttstop);
+            this.Controls.Add(this.bttstart);
+            this.Name = "XtraForm1";
+            this.Text = "XtraForm1";
+            this.Load += new System.EventHandler(this.XtraForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton bttstart;
+        private DevExpress.XtraEditors.SimpleButton bttstop;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+    }
+}
---
+++ XtraForm1.cs
@@ -0,0 +1,190 @@
+﻿using DevExpress.ClipboardSource.SpreadsheetML;
+using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WinPruebas
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        private string fe_start;
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void XtraForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttstart_Click(object sender, EventArgs e)
+        {
+            fe_start = "run";
+            memoEdit1.Text = "start\r\n";
+            // Reset the text in the result label.
+            //resultLabel.Text = String.Empty;
+
+            // Disable the UpDown control until
+            // the asynchronous operation is done.
+            //this.numericUpDown1.Enabled = false;
+
+            // Disable the Start button until
+            // the asynchronous operation is done.
+            //this.startAsyncButton.Enabled = false;
+
+            // Enable the Cancel button while
+            // the asynchronous operation runs.
+            //this.cancelAsyncButton.Enabled = true;
+
+            // Get the value from the UpDown control.
+            //numberToCompute = (int)numericUpDown1.Value;
+
+            // Reset the variable for percentage tracking.
+            //highestPercentageReached = 0;
+
+            // Start the asynchronous operation.
+            if (backgroundWorker1.IsBusy != true)
+                backgroundWorker1.RunWorkerAsync();
+            //backgroundWorker1.RunWorkerAsync(numberToCompute);
+
+        }
+
+        private void bttstop_Click(object sender, EventArgs e)
+        {
+            fe_start = "stop";
+            // Cancel the asynchronous operation.
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+                this.backgroundWorker1.CancelAsync();
+
+            // Disable the Cancel button.
+            //cancelAsyncButton.Enabled = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ////e.Result = ejecutarFE();
+            int fila = 0;
+            //while (fe_start == "run")
+            //{
+            //    System.Threading.Thread.Sleep(5000);
+            //    fila++;
+
+            //    //richTextBox1.Text = fe_start + " " + fila.ToString();
+            //    //memoEdit1.Text = fe_start + " " + fila.ToString();
+            //    Console.WriteLine("Fila = " + fila);
+            //    e.Result = fe_start + " " + fila.ToString();
+            //}
+            string texto = "Proceso = ";
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (fe_start == "run")
+            {
+                fila++;
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    texto = VerificarConexionURL("https://cel.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOfflin?wsdl");
+                    // Perform a time consuming operation and report progress.
+                    //System.Threading.Thread.Sleep(500);
+                    worker.ReportProgress(fila, texto);
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // First, handle the case where an exception was thrown.
+            if (e.Error != null)
+            {
+                memoEdit1.Text += e.Error.Message;
+            }
+            else if (e.Cancelled)
+            {
+                // Next, handle the case where the user canceled
+                // the operation.
+                // Note that due to a race condition in
+                // the DoWork event handler, the Cancelled
+                // flag may not have been set, even though
+                // CancelAsync was called.
+                memoEdit1.Text += "Canceled";
+            }
+            else
+            {
+                // Finally, handle the case where the operation
+                // succeeded.
+                memoEdit1.Text += "Finalizado"; //e.Result.ToString();
+            }
+            memoEdit1.SelectionStart = memoEdit1.Text.Length;
+            memoEdit1.MaskBox.MaskBoxScrollToCaret();
+
+            //// Enable the UpDown control.
+            //this.numericUpDown1.Enabled = true;
+
+            //// Enable the Start button.
+            //startAsyncButton.Enabled = true;
+
+            //// Disable the Cancel button.
+            //cancelAsyncButton.Enabled = false;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string valor = (string)e.UserState;
+            richTextBox1.Text += valor + (e.ProgressPercentage.ToString() + "%") + "\r\n";
+            memoEdit1.Text += valor + (e.ProgressPercentage.ToString() + "%") + "\r\n";
+
+            memoEdit1.SelectionStart = memoEdit1.Text.Length;
+            memoEdit1.MaskBox.MaskBoxScrollToCaret();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            int fila = 0;
+            for(int i=0; i<10; i++)
+            {
+                System.Threading.Thread.Sleep(5000);
+                fila++;
+
+                Console.WriteLine("Fila = " + i);
+            }
+        }
+
+        public string VerificarConexionURL(string mURL)
+        {
+            string resp = "";
+            System.Net.WebRequest Peticion = default(System.Net.WebRequest);
+            System.Net.HttpWebResponse Respuesta;// = default(System.Net.WebResponse);
+            try
+            {
+                Peticion = System.Net.WebRequest.Create(mURL);
+                Respuesta = (HttpWebResponse)Peticion.GetResponse();
+                resp = "true";
+            }
+            catch (System.Net.WebException ex)
+            {
+                if (ex.Status == System.Net.WebExceptionStatus.NameResolutionFailure)
+                {
+                    //return false;
+                    resp = ex.Status.ToString();
+                }
+                resp = ex.Message;
+            }
+            return resp;
+        }
+    }
+}
