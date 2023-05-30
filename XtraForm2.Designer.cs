56dd19df41cbb35d6d9f3e5de987da64e046f19a
Paul Angamarca Pinos <113148324+Paul77711@users.noreply.github.com>
2023-05-30 12:53:23 -0500

    nuevos controles Xform2

--- XtraForm2.Designer.cs
+++ XtraForm2.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dashboardViewer1 = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            this.ratingControl1 = new DevExpress.XtraEditors.RatingControl();
+            this.colorEdit1 = new DevExpress.XtraEditors.ColorEdit();
             ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ratingControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // dashboardViewer1
@@ -40,27 +44,56 @@
             this.dashboardViewer1.AsyncMode = true;
             this.dashboardViewer1.DashboardSource = "C:/Users/Sistemas/Documents/Visual%20Studio%202022/Proyectos/WinPruebas/DXApplica" +
     "tion1/bin/Debug/DashboardsFaustoPrueba.xml";
-            this.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dashboardViewer1.Location = new System.Drawing.Point(0, 0);
+            this.dashboardViewer1.Location = new System.Drawing.Point(1, 12);
             this.dashboardViewer1.Name = "dashboardViewer1";
-            this.dashboardViewer1.Size = new System.Drawing.Size(846, 331);
+            this.dashboardViewer1.Size = new System.Drawing.Size(586, 318);
             this.dashboardViewer1.TabIndex = 0;
+            // 
+            // ratingControl1
+            // 
+            this.ratingControl1.Location = new System.Drawing.Point(646, 12);
+            this.ratingControl1.Name = "ratingControl1";
+            this.ratingControl1.Rating = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.ratingControl1.Size = new System.Drawing.Size(92, 16);
+            this.ratingControl1.TabIndex = 1;
+            this.ratingControl1.Text = "ratingControl1";
+            // 
+            // colorEdit1
+            // 
+            this.colorEdit1.EditValue = System.Drawing.Color.Empty;
+            this.colorEdit1.Location = new System.Drawing.Point(646, 53);
+            this.colorEdit1.Name = "colorEdit1";
+            this.colorEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.colorEdit1.Size = new System.Drawing.Size(140, 20);
+            this.colorEdit1.TabIndex = 2;
             // 
             // XtraForm2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(846, 331);
+            this.Controls.Add(this.colorEdit1);
+            this.Controls.Add(this.ratingControl1);
             this.Controls.Add(this.dashboardViewer1);
             this.Name = "XtraForm2";
             this.Text = "XtraForm2";
             ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ratingControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DevExpress.DashboardWin.DashboardViewer dashboardViewer1;
+        private DevExpress.XtraEditors.RatingControl ratingControl1;
+        private DevExpress.XtraEditors.ColorEdit colorEdit1;
     }
 }
